506b097911bad4d929cd0237ca378e4b090940a1
Ronak Mutha <mutharonak97@gmail.com>
2021-09-20 20:38:51 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# is your birthday lucky
- Website to check whether your birthday is lucky or not
+# Is your birthday lucky?
+ Website to check whether your birthday is lucky or not. 
+
+## How it works?
+This website takes user's birthdate and lucky number as input and then take the sum of digits of birthdate and divides it by user's lucky number.
+
+**Language used**: HTML, CSS, and JavaScript.
---
+++ index.js
@@ -0,0 +1,37 @@
+const dateOfBirth = document.querySelector('#date-of-birth');
+const luckyNumber = document.querySelector('#lucky-number');
+const checkNumberBtn = document.querySelector('#check-number-btn');
+const displayMessage = document.querySelector('#message');
+
+const calculateSum = (dob) => {
+    let sum = 0;
+    dob = dob.replaceAll("-", "");
+    console.log(dob);
+    for (let digit of dob) {
+        sum = sum + Number(digit);
+      }
+    return sum;
+};
+
+const checkIsNumberLucky = (sum, number) => {
+    console.log(sum, number);
+    if( sum % number == 0) {
+        return showMessage(`${number} is a lucky number!! 🥳 🥳 🥳 `);
+    }
+    showMessage(`${number} is not that lucky 😕`);
+};
+
+const showMessage = (message) => {
+    displayMessage.innerText = message;
+};
+
+checkNumberBtn.addEventListener('click',() => {
+    const dob = dateOfBirth.value;const number = luckyNumber.value;
+    if(dob && number) {
+        const sum = calculateSum(dob);
+        checkIsNumberLucky(sum, number);
+    } else {
+        showMessage("Please enter both the fields!!");
+    }
+});
+
